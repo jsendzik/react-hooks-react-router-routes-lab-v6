4cbbce04ff05696037981b0abf10aedc01ed1b86
Jacob Sendzik <jake.sendzik@gmail.com>
2023-11-17 14:26:08 -0500

    Complete

--- src/pages/Directors.js
+++ src/pages/Directors.js
@@ -1,13 +1,35 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
+import { useParams } from "react-router-dom";
 
 function Directors() {
+  const [directors, setDirectors] = useState([]);
+  
+  useEffect(()=> {
+    fetch("http://localhost:4000/directors")
+    .then((r)=> r.json())
+    .then((directors)=> setDirectors(directors))
+  })
+
+  const articles = directors.map((director) => {
+    return(
+      <article key={director.id}>
+        <h2>{director.name}</h2>
+        <ul>{director.movies.map((movie) => {
+          return <li key={movie}>{movie}</li>
+        })}</ul>
+      </article>
+    )
+  })
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Director info here! */}
+        <h1>Directors Page</h1>
+        {articles}
       </main>
     </>
   );
